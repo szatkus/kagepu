2994a958b0ee537744b3837ef71cfcd931d8a53d
Tomasz Szatkowski <szatkus@gmail.com>
2019-03-08 20:06:30 +0100

    Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: 'kagepu.js',
+    path: __dirname,
+    library: 'kagepu',
+    libraryTarget: 'var',
+    libraryExport: 'default'
+  }
+}
