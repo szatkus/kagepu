ed83ddc6af34e67341127f82cbbc9e25784991ba
Tomasz Szatkowski <tomasz.szatkowski@baroo.ie>
2019-07-14 12:13:53 +0200

    List structure of parser tree

--- wtest.js
+++ wtest.js
@@ -1,11 +1,23 @@
-const antlr4 = require('antlr4');
-const WHLSLLexer = require('./grammar/WHLSLLexer.js');
-const WHLSLParser = require('./grammar/WHLSLParser.js');
+const antlr4 = require('antlr4')
+const WHLSLLexer = require('./grammar/WHLSLLexer.js')
+const WHLSLParser = require('./grammar/WHLSLParser.js')
 
-const chars = new antlr4.FileStream('a.whlsl');
-const lexer = new WHLSLLexer.WHLSLLexer(chars);
+const chars = new antlr4.FileStream('a.whlsl')
+const lexer = new WHLSLLexer.WHLSLLexer(chars)
 
-const tokens = new antlr4.CommonTokenStream(lexer);
-const parser = new WHLSLParser.WHLSLParser(tokens);
+const tokens = new antlr4.CommonTokenStream(lexer)
+const parser = new WHLSLParser.WHLSLParser(tokens)
 
-console.log(parser.file());
+let level = 0
+
+function visit(node) {
+    console.log('  '.repeat(level) + node.constructor.name)
+    
+    if (node.children) {
+        level += 1
+        node.children.forEach(visit)
+        level -= 1
+    }
+}
+
+visit(parser.file())
