5e4247612f110eb42c18fb12d699d14f5ba97116
Tomasz Szatkowski <szatkus@gmail.com>
2019-12-14 18:30:25 +0100

    OpVectorSuffle

--- src/spirv/vectors.ts
+++ src/spirv/vectors.ts
@@ -1,21 +1,38 @@
 import { CompilationState, CompiledModule } from "./compilation";
 import { Execution } from "./execution";
 import dontKnow from "../dontKnow";
+import { Pointer, Memory } from "./memory";
+import { TypeVector, TypeFloat } from "./types";
 
 export function compile (state: CompilationState, module: CompiledModule) {
     switch(state.opCode) {
         // OpVectorShuffle
         case 79:
             {
-                let resultType = state.consumeWord()
+                let resultTypeId = state.consumeWord()
                 let resultId = state.consumeWord()
-                let leftVector = state.consumeWord()
-                let rightVector = state.consumeWord()
-                let components = state.consumeArray
+                let leftVectorId = state.consumeWord()
+                let rightVectorId = state.consumeWord()
+                let components = state.consumeArray()
                 module.flow.push((execution: Execution) => {
-                    dontKnow()
+                    const UNDEFINED = 0xFFFFFFFF
+                    let resultType = <TypeVector> execution.heap[resultTypeId]
+                    if (!(resultType.type instanceof TypeFloat) || resultType.type.width !== 32) {
+                        dontKnow()
+                    }
+                    let leftVector = <Pointer> execution.heap[leftVectorId]
+                    let rightVector = <Pointer> execution.heap[rightVectorId]
+                    let result = new Float32Array(resultType.count)
+                    components.forEach((component, index) => {
+                        if (index % 2 == 0) {
+                            result[index] = leftVector.readFloat32Array()[component]
+                        } else {
+                            result[index] = rightVector.readFloat32Array()[component]
+                        }
+                    })
+                    execution.heap[resultId] = new Pointer(new Memory(result.buffer), 0, resultType)
                 })
-                console.debug(`$${resultId} = OpVectorShuffle ${leftVector} ${rightVector}`)
+                console.debug(`$${resultId} = OpVectorShuffle ${leftVectorId} ${rightVectorId}`)
                 state.processed = true
             }
         break
