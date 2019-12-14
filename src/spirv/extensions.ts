466542cb53ea0f175ccfdc899b5d456202f6c6c5
Tomasz Szatkowski <szatkus@gmail.com>
2019-12-14 18:29:52 +0100

    Implement normalization as an external instruction

--- src/spirv/extensions.ts
+++ src/spirv/extensions.ts
@@ -1,6 +1,8 @@
 import { CompilationState, CompiledModule } from "./compilation";
 import { Execution } from "./execution";
 import dontKnow from "../dontKnow";
+import { TypeVector, TypeFloat } from "./types";
+import { Pointer, Memory } from "./memory";
 
 export function compile (state: CompilationState, module: CompiledModule) {
     switch(state.opCode) {
@@ -19,12 +21,27 @@ export function compile (state: CompilationState, module: CompiledModule) {
         // OpExtInst
         case 12:
             {
-                let resultType = state.consumeWord()
+                let resultTypeId = state.consumeWord()
                 let resultId = state.consumeWord()
-                let set = state.consumeWord()
+                let setId = state.consumeWord()
                 let instruction = state.consumeWord()
-                let operands = state.consumeArray()
+                let operandsIds = state.consumeArray()
                 module.flow.push((execution: Execution) => {
+                    let resultType = <TypeVector> execution.heap[resultTypeId]
+                    let set = execution.heap[setId]
+                    let operands = operandsIds.map(i => execution.heap[i])
+                    // normalize
+                    if (set === 'GLSL.std.450' && instruction === 69) {
+                        if (!(resultType.type instanceof TypeFloat) || resultType.type.width !== 32) {
+                            dontKnow()
+                        }
+                        let maxValue = 0
+                        let vector = (<Pointer> operands[0]).readFloat32Array()
+                        vector.forEach(value => maxValue = Math.max(Math.abs(value), maxValue))
+                        let result = new Float32Array(vector.map(value => value / maxValue))
+                        execution.heap[resultId] = new Pointer(new Memory(result.buffer), 0, resultType)
+                        return
+                    }
                     dontKnow()
                 })
                 console.debug(`OpExtInstImport ${name}`)
