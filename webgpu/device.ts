2faf2d6545577077d0a8f91279ff5bdbd20afa6d
Tomasz Szatkowski <szatkus@gmail.com>
2020-01-11 20:37:18 +0100

    Create an error reporter

--- webgpu/device.ts
+++ webgpu/device.ts
@@ -5,7 +5,7 @@ import GPUCommandEncoder from './GPUCommandEncoder'
 import { KBindGroupLayout, KBindGroup } from './bindGroups'
 import KQueue from './KQueue'
 import { KComputePipeline } from './compute'
-import { KValidationError } from './errors'
+import { ErrorReporter } from './errors'
 import { GPURenderBundleEncoder } from './GPURenderBundleEncoder'
 import { KShaderModule } from './shaders'
 import { KRenderPipeline } from './render'
@@ -22,15 +22,15 @@ export class KPipelineLayout implements GPUPipelineLayout {
   constructor (public _descriptor: GPUPipelineLayoutDescriptor) {
 
   }
+
 }
 
 export class GPUDevice {
   public extensions = extensions
   public limit = limits
   public adapter: GPUAdapter
-  private _filters: GPUErrorFilter = 'none'
-  private _error?: GPUError
   public defaultQueue = new KQueue()
+  private _errorReporter = new ErrorReporter()
 
   constructor (adapter: GPUAdapter) {
     this.adapter = adapter
@@ -61,13 +61,13 @@ export class GPUDevice {
   }
 
   createBindGroupLayout (descriptor: GPUBindGroupLayoutDescriptor): GPUBindGroupLayout {
-    if (this._validation()) {
+    if (this._errorReporter.validation()) {
       let locations = new Set()
       let storageBuffers = 0
       let uniformBuffers = 0
       for (let layout of descriptor.bindings ?? []) {
         if (locations.has(layout.binding) || layout.binding < 0) {
-          this._error = new GPUValidationError('Incorrect binding.')
+          this._errorReporter.createValidationError('Incorrect binding.')
         }
         if (layout.type === 'storage-buffer') {
           storageBuffers++
@@ -76,22 +76,22 @@ export class GPUDevice {
           uniformBuffers++
         }
         if (layout.hasDynamicOffset && layout.type !== 'storage-buffer' && layout.type !== 'readonly-storage-buffer' && layout.type !== 'uniform-buffer') {
-          this._error = new GPUValidationError('Dynamic offset is allowed only for buffers.')
+          this._errorReporter.createValidationError('Dynamic offset is allowed only for buffers.')
         }
         locations.add(layout.binding)
       }
       if (storageBuffers > 4) {
-        this._error = new GPUValidationError('Too many storage buffers.')
+        this._errorReporter.createValidationError('Too many storage buffers.')
       }
       if (uniformBuffers > 8) {
-        this._error = new GPUValidationError('Too many uniform buffers.')
+        this._errorReporter.createValidationError('Too many uniform buffers.')
       }
     }
     return new KBindGroupLayout(descriptor)
   }
 
   createPipelineLayout (descriptor: GPUPipelineLayoutDescriptor): GPUPipelineLayout {
-    if (this._validation()) {
+    if (this._errorReporter.validation()) {
       let storageBuffers = 0
       let uniformBuffers = 0
       for (let layout of descriptor.bindGroupLayouts as KBindGroupLayout[]) {
@@ -99,13 +99,13 @@ export class GPUDevice {
         uniformBuffers += layout._getBindingsByType('uniform-buffer').length
       }
       if (storageBuffers > 4) {
-        this._error = new GPUValidationError('Too many storage buffers.')
+        this._errorReporter.createValidationError('Too many storage buffers.')
       }
       if (uniformBuffers > 8) {
-        this._error = new GPUValidationError('Too many uniform buffers.')
+        this._errorReporter.createValidationError('Too many uniform buffers.')
       }
       if (descriptor.bindGroupLayouts.length > 4) {
-        this._error = new GPUValidationError('Too many bind group layouts.')
+        this._errorReporter.createValidationError('Too many bind group layouts.')
       }
     }
     return new KPipelineLayout(descriptor)
@@ -116,26 +116,40 @@ export class GPUDevice {
   }
 
   createRenderPipeline (descriptor: GPURenderPipelineDescriptor): GPURenderPipeline {
-    if (this._validation() && descriptor.vertexState) {
-      if ((descriptor.vertexState.vertexBuffers?.length ?? 0) > 16) {
-        this._error = new GPUValidationError('Too many vertex buffers.')
+    if (this._errorReporter.validation()) {
+      if (descriptor.sampleCount !== 1 && descriptor.sampleCount !== 4) {
+        this._errorReporter.createValidationError('Sample count is incorrect.')
       }
-      if ((descriptor.vertexState.vertexBuffers ?? []).map(v => v.attributes.length).reduce((a, b) => a + b, 0) > 16) {
-        this._error = new GPUValidationError('Too many attributes.')
+      if (!descriptor.depthStencilState && descriptor.colorStates.length === 0) {
+        this._errorReporter.createValidationError('At least one color state is required.')
       }
-      let locations = new Set()
-      for (let vertexBuffer of descriptor.vertexState.vertexBuffers || []) {
-        if ((vertexBuffer.arrayStride ?? 0) > 2048) {
-          this._error = new GPUValidationError('Too large array stride.')
+      for (let colorState of descriptor.colorStates) {
+        if (colorState.format === 'r8snorm' || colorState.format === 'rg8snorm' || colorState.format === 'rgba8snorm' || colorState.format === 'rg11b10float' || 
+            colorState.format === 'depth32float' || colorState.format === 'depth24plus' || colorState.format === 'depth24plus-stencil8') {
+          this._errorReporter.createValidationError('Color format is not renderable.')
         }
-        for (let attribute of vertexBuffer.attributes) {
-          if ((vertexBuffer.arrayStride ?? 0) > 0 && attribute.offset >= vertexBuffer.arrayStride) {
-            this._error = new GPUValidationError('Attribute offset cannot exceed vertex buffer array stride.')
+      }
+      if (descriptor.vertexState) {
+        if ((descriptor.vertexState.vertexBuffers?.length ?? 0) > 16) {
+          this._errorReporter.createValidationError('Too many vertex buffers.')
+        }
+        if ((descriptor.vertexState.vertexBuffers ?? []).map(v => v.attributes.length).reduce((a, b) => a + b, 0) > 16) {
+          this._errorReporter.createValidationError('Too many attributes.')
+        }
+        let locations = new Set()
+        for (let vertexBuffer of descriptor.vertexState.vertexBuffers || []) {
+          if ((vertexBuffer.arrayStride ?? 0) > 2048) {
+            this._errorReporter.createValidationError('Too large array stride.')
           }
-          if (locations.has(attribute.shaderLocation)) {
-            this._error = new GPUValidationError('Multiple attributes in one location.')
+          for (let attribute of vertexBuffer.attributes) {
+            if ((vertexBuffer.arrayStride ?? 0) > 0 && attribute.offset >= vertexBuffer.arrayStride) {
+              this._errorReporter.createValidationError('Attribute offset cannot exceed vertex buffer array stride.')
+            }
+            if (locations.has(attribute.shaderLocation)) {
+              this._errorReporter.createValidationError('Multiple attributes in one location.')
+            }
+            locations.add(attribute.shaderLocation)
           }
-          locations.add(attribute.shaderLocation)
         }
       }
     }
@@ -147,76 +161,76 @@ export class GPUDevice {
   }
 
   createBindGroup (descriptor: GPUBindGroupDescriptor): GPUBindGroup {
-    if (this._validation()) {
+    if (this._errorReporter.validation()) {
       let layout = descriptor.layout as KBindGroupLayout
       if (layout._getBindingsCount() !== descriptor.bindings.length) {
-        this._error = new GPUValidationError('Bindings count mismatch.')
+        this._errorReporter.createValidationError('Bindings count mismatch.')
       }
       for (let i = 0; i < descriptor.bindings.length; i++) {
         let binding = descriptor.bindings[i]
         let bindingLayout = layout._getBindings(i)
         if (binding.binding >= descriptor.bindings.length) {
-          this._error = new GPUValidationError('Incorrect binding location.')
+          this._errorReporter.createValidationError('Incorrect binding location.')
         }
         let bufferBinding = binding.resource as GPUBufferBinding
         if (bufferBinding.buffer instanceof KBuffer) {
           if (!bufferBinding.buffer._isCorrect()) {
-            this._error = new GPUValidationError('Buffer is messed up.')
+            this._errorReporter.createValidationError('Buffer is messed up.')
           }
 
           if (bindingLayout) {
             if (bindingLayout.type === 'uniform-buffer' && !bufferBinding.buffer._isUniform()) {
-              this._error = new GPUValidationError('Buffer is not uniform.')
+              this._errorReporter.createValidationError('Buffer is not uniform.')
             }
             if (bindingLayout.type === 'storage-buffer' && !bufferBinding.buffer._isStorage()) {
-              this._error = new GPUValidationError('Buffer is not storage.')
+              this._errorReporter.createValidationError('Buffer is not storage.')
             }
             if (bindingLayout.type === 'readonly-storage-buffer' && !bufferBinding.buffer._isStorage() && !bufferBinding.buffer._isReadonly()) {
-              this._error = new GPUValidationError('Buffer is not correct.')
+              this._errorReporter.createValidationError('Buffer is not correct.')
             }
             if (bindingLayout.type !== 'storage-buffer' && bindingLayout.type !== 'readonly-storage-buffer' && bindingLayout.type !== 'uniform-buffer') {
-              this._error = new GPUValidationError('Buffer is not a correct type.')
+              this._errorReporter.createValidationError('Buffer is not a correct type.')
             }
           }
 
           if ((bufferBinding.offset ?? 0) > bufferBinding.buffer._getArrayBuffer().byteLength ||
             (bufferBinding.size ?? 0) > bufferBinding.buffer._getArrayBuffer().byteLength ||
             (bufferBinding.offset ?? 0) + (bufferBinding.size ?? 0) > bufferBinding.buffer._getArrayBuffer().byteLength) {
-            this._error = new GPUValidationError('Offset is misaligned.')
+            this._errorReporter.createValidationError('Offset is misaligned.')
           }
 
           if (!(bufferBinding.offset === undefined && bufferBinding.size === undefined) &&
               bufferBinding.offset !== 0 && bufferBinding.size !== 0 &&
               (bufferBinding.offset ?? 0) % (bufferBinding.size ?? 0) !== 0) {
-            this._error = new GPUValidationError('Offset is misaligned.')
+            this._errorReporter.createValidationError('Offset is misaligned.')
           }
 
           if ((bufferBinding.size ?? 0) < 0) {
-            this._error = new GPUValidationError('Offset is misaligned.')
+            this._errorReporter.createValidationError('Offset is misaligned.')
           }
         }
         if (binding.resource instanceof KSampler && bindingLayout) {
           if (bindingLayout.type !== 'sampler') {
-            this._error = new GPUValidationError('Sampler is not a correct type.')
+            this._errorReporter.createValidationError('Sampler is not a correct type.')
           }
         }
         if (binding.resource instanceof KTextureView && bindingLayout) {
           if (bindingLayout.type === 'sampled-texture' && !binding.resource._isSampled()) {
-            this._error = new GPUValidationError('Texture is not sampled.')
+            this._errorReporter.createValidationError('Texture is not sampled.')
           }
           if (bindingLayout.type === 'storage-texture' && !binding.resource._isStorage()) {
-            this._error = new GPUValidationError('Texture is not storage.')
+            this._errorReporter.createValidationError('Texture is not storage.')
           }
           if (bindingLayout.type !== 'sampled-texture' && bindingLayout.type !== 'storage-texture') {
-            this._error = new GPUValidationError('Texture is not a correct type.')
+            this._errorReporter.createValidationError('Texture is not a correct type.')
           }
           if ((bindingLayout.textureComponentType === 'float' && binding.resource._getFormat().indexOf('unorm') === -1) ||
               (bindingLayout.textureComponentType === 'sint' && binding.resource._getFormat().indexOf('r8sint') === -1) ||
               (bindingLayout.textureComponentType === 'uint' && binding.resource._getFormat().indexOf('r8uint') === -1)) {
-            this._error = new GPUValidationError('Incorrect format.')
+            this._errorReporter.createValidationError('Incorrect format.')
           }
           if (binding.resource._texture._getArrayLayerCount() !== 1) {
-            this._error = new GPUValidationError('Incorrect dimensions.')
+            this._errorReporter.createValidationError('Incorrect dimensions.')
           }
         }
       }
@@ -233,13 +247,11 @@ export class GPUDevice {
   }
 
   pushErrorScope (filter: GPUErrorFilter) {
-    this._filters = filter
+    this._errorReporter.pushErrorScope(filter)
   }
 
   async popErrorScope (): Promise<GPUError | null> {
-    let error = this._error
-    delete this._error
-    return error || null
+    return this._errorReporter.popErrorScope()
   }
 
   /*
@@ -249,9 +261,6 @@ export class GPUDevice {
     return this.defaultQueue
   }
 
-  _validation (): boolean {
-    return this._filters === 'validation'
-  }
 }
 
 export class GPUAdapter {
--- webgpu/errors.ts
+++ webgpu/errors.ts
@@ -6,3 +6,28 @@ export class KValidationError implements GPUValidationError {
 export class KOutOfMemoryError implements GPUOutOfMemoryError {
   constructor (public message: string) {}
 }
+
+export class ErrorReporter {
+  private error?: GPUError
+  filter: GPUErrorFilter = 'none'
+
+  createValidationError (message: string) {
+    this.error = new GPUValidationError(message)
+  }
+
+  // TODO: OOM errors
+
+  pushErrorScope (filter: GPUErrorFilter) {
+    this.filter = filter
+  }
+
+  async popErrorScope (): Promise<GPUError | null> {
+    let error = this.error
+    delete this.error
+    return error || null
+  }
+
+  validation (): boolean {
+    return this.filter === 'validation'
+  }
+}
